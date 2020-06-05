77bdf58dd6fabd1ae233b376e3541fd60008139a
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-05 21:54:06 +0200

    unicafe 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,52 +1,50 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+
+  const getAverage = () => {
+    const totalScore = good - bad
+    return totalScore / getTotalVotes()
+  }
+
+  const getPositive = () => {
+    return (good / getTotalVotes()) * 100
+  }
+
+  const getTotalVotes = () => {
+    return good + neutral + bad
+  }
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {getTotalVotes()}</p>
+      <p>average {getAverage()}</p>
+      <p>postive {getPositive()}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [votes, setVotes] = useState(0)
-
-  const handleClickGood = () => {
-    setGood(good + 1)
-    setVotes(votes + 1)
-  }
-
-  const handleClickNeutral = () => {
-    setNeutral(neutral + 1)
-    setVotes(votes + 1)
-  }
-
-  const handleClickBad = () => {
-    setBad(bad + 1)
-    setVotes(votes + 1)
-  }
-
-  const getAverage = () => {
-    const totalScore = good - bad
-    return totalScore / votes
-  }
-
-  const getPositive = () => {
-    return (good / votes) * 100
-  }
 
   return (
     <div>
       <h2>give feedback</h2>
       <div>
-        <button onClick={handleClickGood}>good</button>
-        <button onClick={handleClickNeutral}>neutral</button>
-        <button onClick={handleClickBad}>bad</button>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {votes}</p>
-      <p>average {getAverage()}</p>
-      <p>postive {getPositive()}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
