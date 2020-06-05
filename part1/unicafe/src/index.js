dbc4941a27686a0e0195d3c0eb312b7711087146
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-05 21:44:04 +0200

    unicafe 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,19 +6,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [votes, setVotes] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1)
+    setVotes(votes + 1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+    setVotes(votes + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+    setVotes(votes + 1)
+  }
+
+  const getAverage = () => {
+    const totalScore = good - bad
+    return totalScore / votes
+  }
+
+  const getPositive = () => {
+    return (good / votes) * 100
+  }
 
   return (
     <div>
       <h2>give feedback</h2>
       <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <button onClick={handleClickGood}>good</button>
+        <button onClick={handleClickNeutral}>neutral</button>
+        <button onClick={handleClickBad}>bad</button>
       </div>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {votes}</p>
+      <p>average {getAverage()}</p>
+      <p>postive {getPositive()}%</p>
     </div>
   )
 }
