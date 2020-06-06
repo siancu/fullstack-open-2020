e7e7bb77a58205b1253d3c9f273e8b12e426684a
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-06 23:11:14 +0200

    countries 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -7,6 +7,7 @@ function App() {
   // state
   const [countries, setCountries] = useState([])
   const [searchCountry, setSearchCountry] = useState('')
+  const [weather, setWeather] = useState(undefined)
 
   // effects
   useEffect(() => {
@@ -23,6 +24,22 @@ function App() {
     }
   }, [searchCountry])
 
+  useEffect(() => {
+    if (countries.length === 1) {
+      const params = {
+        access_key: process.env.REACT_APP_API_KEY,
+        query: countries[0].capital,
+        units: 'm'
+      }
+
+      axios.get('http://api.weatherstack.com/current', {params})
+        .then(response => {
+          setWeather(response.data)
+        })
+    }
+  }, [countries])
+
+
   // logic
   const handleSearchCountry = (event) => {
     setSearchCountry(event.target.value)
@@ -35,12 +52,12 @@ function App() {
     return handler;
   }
 
-  let element = undefined
-  
+  let element
+
   if (countries.length === 0) {
     element = <div></div>
   } else if (countries.length === 1) {
-    element = <Country country={countries[0]} />
+    element = <Country country={countries[0]} weather={weather} />
   } else if (countries.length < 10) {
     const countryNames = countries.map(country => <ButtonWithCountry key={country.name} country={country} clickHandler={handleButtonEvent} />)
     element = <div>{countryNames}</div>
--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
-const Country = ({ country }) => {
+const Country = ({ country, weather }) => {
   const languages = country.languages.map(language => <li key={language.name}>{language.name}</li>)
+  const weatherElement = weather ?
+    <div>
+      <h2>Weather in {country.capital}</h2>
+      <p><b>temperature: </b>{weather.current.temperature} Celsius</p>
+      <img src={weather.current.weather_icons[0]} width={100} height={100} alt='weather icon'/>
+      <p><b>wind: </b>{weather.current.wind_speed} mph direction {weather.current.wind_dir}</p>
+    </div>
+    : <div />
   return (
     <div>
-        <h1>{country.name}</h1>
-        <p>capital {country.capital}</p>
-        <p>population {country.population}</p>
-        <h2>languages</h2>
-        <ul>{languages}</ul>
-        <img src={country.flag} width="200" height="150" alt="country flag" />
-      </div>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h2>languages</h2>
+      <ul>{languages}</ul>
+      <img src={country.flag} width="200" height="150" alt="country flag" />
+      {weatherElement}
+    </div>
   )
 }
 
