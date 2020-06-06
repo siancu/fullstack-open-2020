8d2cdc512e1fc99334e11c5b97d34fcf5d8e173d
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-06 10:28:03 +0200

    courseinfo 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,45 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Total = ({ course }) => {
-  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
-
-  return(
-    <p><b>total of {total} exercises</b></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>  
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
