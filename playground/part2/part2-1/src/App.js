e0a90635742d8c167b89e55120d6342999b21842
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-07 09:40:47 +0200

    Playground.

--- playground/part2/part2-1/src/App.js
+++ playground/part2/part2-1/src/App.js
@@ -37,9 +37,13 @@ const App = (props) => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    noteService.update(id, changedNote)
-      .then(returnedNote => {
+    noteService
+      .update(id, changedNote).then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from the server`)
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
