1f64b4faacdd910ba7b2cec82219431f78de998a
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-09 00:02:08 +0200

    Change the URL to the backend.

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://pure-brushlands-53653.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
