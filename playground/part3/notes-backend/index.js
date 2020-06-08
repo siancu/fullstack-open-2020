80c8f43072a9a9a1b7118e4826c1dd693341f98f
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-08 23:38:09 +0200

    playground: node backend to heroku

--- playground/part3/notes-backend/index.js
+++ playground/part3/notes-backend/index.js
@@ -77,7 +77,7 @@ app.post('/api/notes', (req, res) => {
   res.json(note)
 })
 
-const port = 3001
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
